123ca8c8b4f653f405b929c663ed89e2bff65c4e
Adriana Ochoa <15171173@itculiacan.com>
2019-12-08 15:57:05 -0700

    adding reporte activity

--- T/app/src/main/java/com/example/t/ui/login/Tracker.kt
+++ T/app/src/main/java/com/example/t/ui/login/Tracker.kt
@@ -8,7 +8,7 @@ class Tracker : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tracker_taller)
+        setContentView(R.layout.activity_reporte)
     }
 }
 
