8bf691f874bcdf9307edd9f575b195e8c028923c
Adriana Ochoa <15171173@itculiacan.com>
2019-12-08 18:52:32 -0700

    adding api functions

--- T/app/src/main/java/com/example/t/ui/login/LoginActivity.kt
+++ T/app/src/main/java/com/example/t/ui/login/LoginActivity.kt
@@ -17,6 +17,7 @@ import android.widget.Toast
 import android.content.Intent
 
 import com.example.t.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,114 +28,39 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_login)
 
+        val folio = findViewById<EditText>(R.id.folio)
 
-
-        val username = findViewById<EditText>(R.id.folio)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
-        val loading = findViewById<ProgressBar>(R.id.loading)
-
-        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-
-            // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
-            }
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
-
-            loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
-            }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
-            }
-            setResult(Activity.RESULT_OK)
-
-            //Complete and destroy login activity once successful
-            finish()
-        })
-
-        username.afterTextChanged {
-            loginViewModel.loginDataChanged(
-                username.text.toString(),
-                password.text.toString()
-            )
-        }
-
-        password.apply {
-            afterTextChanged {
-                loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
-                )
-            }
-
-            setOnEditorActionListener { _, actionId, _ ->
-                when (actionId) {
-                    EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                            username.text.toString(),
-                            password.text.toString()
-                        )
-                }
-                false
-            }
+        
 
             login.setOnClickListener {
-                loading.visibility = View.VISIBLE
-                loginViewModel.login(username.text.toString(), password.text.toString())
-                startTrackerActivity()
-
-
+                startTrackerActivity(folio.toString())
             }
+
         }
-    }
 
 
-    private fun startTrackerActivity(){
+
+    private fun startTrackerActivity(folio: String ){
         val intent = Intent(this, Tracker :: class.java)
+        intent.putExtra("folio", folio)
+
         startActivity(intent)
     }
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome)
-        // TODO : initiate successful logged in experience
-        Toast.makeText(
-            applicationContext,
-            "$welcome",
-            Toast.LENGTH_LONG
-        ).show()
+    private fun startTrackerActivityTaller(){
+        val intent = Intent(this, Tracker_taller :: class.java)
+        startActivity(intent)
     }
 
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+    private fun startTrackerActivityTienda(){
+        val intent = Intent(this, Tracker_tienda :: class.java)
+        startActivity(intent)
     }
-}
+    private fun startTrackerActivityReporte(){
+        val intent = Intent(this, Reporte :: class.java)
+        startActivity(intent)
+    }
 
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
 
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
 }
--- T/app/src/main/java/com/example/t/ui/login/Tracker_tienda.kt
+++ T/app/src/main/java/com/example/t/ui/login/Tracker_tienda.kt
@@ -1,15 +1,13 @@
 package com.example.t.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.t.R
 
-class Tracker : AppCompatActivity() {
+class Tracker_tienda : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tracker)
+        setContentView(R.layout.activity_tracker_tienda)
     }
 }
-
-
