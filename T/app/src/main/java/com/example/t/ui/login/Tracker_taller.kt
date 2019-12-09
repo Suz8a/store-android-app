a92a896c834bfeb1de835e5e3f0dd2afcffdb820
Adriana Ochoa <15171173@itculiacan.com>
2019-12-09 12:05:35 -0700

    add imageUrl

--- T/app/src/main/java/com/example/t/ui/login/Tracker_taller.kt
+++ T/app/src/main/java/com/example/t/ui/login/Tracker_taller.kt
@@ -1,7 +1,10 @@
 package com.example.t.ui.login
 
+import com.squareup.picasso.Picasso;
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.t.R
 import kotlinx.android.synthetic.main.activity_tracker_taller.*
@@ -12,6 +15,12 @@ class Tracker_taller : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val folio:String = intent.getStringExtra("folio")
         setContentView(R.layout.activity_tracker_taller)
+        val image = findViewById<ImageView>(R.id.image)
+        val imageUrl: String = "https://webdiag714.blob.core.windows.net/sampleimage/3e25fe7a-f8d2-43e2-ba42-69d6a5ea10b0-.jpg"
+
+        Picasso.get().load(imageUrl).into(image)
+
+
 
         btn_reporte.setOnClickListener{
             startTrackerActivityReporte(folio)
